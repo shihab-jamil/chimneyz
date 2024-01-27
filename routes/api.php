b9d25fac34f473db5a2908825ec528a9c02290d5
emon <ishtiaqueferdous109@gmail.com>
2024-01-27 17:46:05 +0600

    Auth Api Completed

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ContactUsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -18,7 +19,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
+/**
+ * Authentication
+ */
+Route::post('/register', [AuthController::class, 'register']);
+Route::post("/login", [AuthController::class, "login"]);
+Route::get('/logout', [AuthController::class, "logout"]);
 
 /**
  * Contact Us
