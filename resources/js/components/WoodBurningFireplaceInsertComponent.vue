71edc6cd1259bdb57b3ca83d087badd10b1c407c
emon <ishtiaqueferdous109@gmail.com>
2024-01-29 19:52:52 +0600

    Wood Burning Fireplace Insert Component Done

---
+++ resources/js/components/WoodBurningFireplaceInsertComponent.vue
@@ -0,0 +1,227 @@
+<template>
+    <v-row class="pa-md-16 my-md-0 my-10">
+        <v-col md="6" cols="12" class="d-flex justify-end align-center">
+            <v-img src="../images/wood_fireplace_1.jpg" :aspect-ratio="9/5" rounded="xl"/>
+        </v-col>
+        <v-col md="6" cols="12">
+            <div class="flex flex-column pa-10">
+                <h1>Lets Talk About Wood Burning Fireplace Insert</h1>
+                <v-divider class="mb-4 w-100" color="green" style="border-top-width: 5px; opacity: 1;"></v-divider>
+                <p class="text-justify">Get a Free Estimate Now
+                    Are you looking for a new fireplace insert for your home? Chimcare sells and installs a wide variety
+                    of wood burning fireplace inserts at our Seattle, WA; Portland, OR; and Cleveland, OH, locations. We
+                    carry a full line of Regency products with a wide range of sizes and looks to fit your specific
+                    needs. Learn more today by browsing our wood burning insert options below. Inserts come in many
+                    forms, and we want to help you find the perfect one for your home.</p>
+                <br>
+            </div>
+        </v-col>
+    </v-row>
+    <v-row class="pa-md-16 my-md-0 my-10">
+        <v-col md="6" cols="12">
+            <div class="flex flex-column pa-10">
+                <h1>Alterra ® Wood Insert</h1>
+                <v-divider class="mb-4 w-100" color="green" style="border-top-width: 5px; opacity: 1;"></v-divider>
+                <p class="text-justify">Transform your fireplace to receive heat~
+
+                    This is a great affordable unit to convert your existing wood burning fireplace into a fireplace
+                    that puts out heat to heat your home. This new contemporary design has a sleek low profile look with
+                    a large glass viewing areas to see that beautiful fire.
+
+                    2 speed blower (included) will distribute heat into the farthest corners of your room.
+                </p>
+                <br>
+                <p><strong>SPECIFICATIONS</strong></p>
+                <br>
+                <p><strong>Input</strong>(BTU) = 55,000</p>
+                <p><strong>Typical SQ.FT Heated</strong>(BTU) = 600 to 1,000 sq.ft</p>
+                <p><strong>Optimum Efficiency</strong> = 77%</p>
+                <p><strong>Maximum Log Size</strong> = 18″</p>
+                <br>
+                <p><strong>Minimum Fireplace Opening</strong></p>
+                <br>
+                <p><strong>Width</strong> = 25″</p>
+                <p><strong>Width</strong> = 23″</p>
+                <p><strong>Depth</strong> = 16″</p>
+            </div>
+        </v-col>
+        <v-col md="6" cols="12" class="d-flex justify-end align-center">
+            <v-img src="../images/alterra_wood_insert.jpg" :aspect-ratio="9/5" rounded="xl"/>
+        </v-col>
+    </v-row>
+    <v-row class="pa-md-16 my-md-0 my-10">
+        <v-col md="6" cols="12" class="d-flex justify-end align-center">
+            <v-img src="../images/pro_series _wood_insert.jpg" :aspect-ratio="9/5" rounded="xl"/>
+        </v-col>
+        <v-col md="6" cols="12">
+            <div class="flex flex-column pa-10">
+                <h1>Pro-Series Wood Insert</h1>
+                <v-divider class="mb-4 w-100" color="green" style="border-top-width: 5px; opacity: 1;"></v-divider>
+                <p class="text-justify">This inserts flush design takes the wood insert category to a whole new level.
+                    Clean lines surround an expansive linear firebox to provide you with a designer look around an
+                    impressive fire.
+
+                    LATEST IN WOOD BURNING TECHNOLOGY
+
+                    This indoor wood fireplace insert uses modern technology to create an efficient, beautiful fireplace
+                    at home. The Eco-Boost hybrid catalytic system burns fuel at a slow rate, delivering 14 hours or
+                    more of heat with a single load of wood! This is one of the best wood burning inserts on the market
+                    today. Fuel is used to its full potential with only 1.8 g/hr of emissions and 86.1% efficiency!
+                </p>
+                <br>
+                <p><strong>SPECIFICATIONS</strong></p>
+                <br>
+                <p><strong>Technology</strong> = Eco-Boost</p>
+                <p><strong>Maximum</strong>(BTU) = 77,000</p>
+                <p><strong>Typical SQ.FT Heated</strong> = up to 2,600 sq.ft</p>
+                <p><strong>Optimum Efficiency</strong> = 86.1%</p>
+                <p><strong>Maximum Log Size</strong> = 22″</p>
+                <br>
+                <p><strong>Minimum Fireplace Opening</strong></p>
+                <br>
+                <p><strong>Width</strong> = 29″</p>
+                <p><strong>Width</strong> = 22″</p>
+                <p><strong>Depth</strong> = 19″</p>
+            </div>
+        </v-col>
+    </v-row>
+    <v-row class="pa-md-16 my-md-0 my-10">
+        <v-col md="6" cols="12">
+            <div class="flex flex-column pa-10">
+                <h1>Medium Classic Free Standing Wood Stove</h1>
+                <v-divider class="mb-4 w-100" color="green" style="border-top-width: 5px; opacity: 1;"></v-divider>
+                <p class="text-justify">The timeless styling of the Classic Wood Stove comes in three sizes producing
+                    55,000 to 80,000 BTU’s. Up to 77% efficiency means any room can be heated quickly. It also means
+                    your wood stove burns cleaner and meets the strictest air quality standards set by the USA and EPA.
+                </p>
+                <br>
+                <p><strong>SPECIFICATIONS</strong></p>
+                <br>
+                <p><strong>Maximum</strong>(BTU) = 75,000</p>
+                <p><strong>Typical SQ.FT Heated</strong> = up to 2,000 sq.ft</p>
+                <p><strong>Optimum Efficiency</strong> = 77%</p>
+                <p><strong>Maximum Log Size</strong> = 18″</p>
+            </div>
+        </v-col>
+        <v-col md="6" cols="12" class="d-flex justify-end align-center">
+            <v-img src="../images/medium_standing_wood_stove.jpg" :aspect-ratio="9/5" rounded="xl"/>
+        </v-col>
+    </v-row>
+    <v-row class="pa-md-16 my-md-0 my-10">
+        <v-col md="6" cols="12" class="d-flex justify-end align-center">
+            <v-img src="../images/hampton_standing_wood_stove.jpg" :aspect-ratio="9/5" rounded="xl"/>
+        </v-col>
+        <v-col md="6" cols="12">
+            <div class="flex flex-column pa-10">
+                <h1>Hampton Free Standing Wood Stove</h1>
+                <v-divider class="mb-4 w-100" color="green" style="border-top-width: 5px; opacity: 1;"></v-divider>
+                <p class="text-justify">Our wood burning stove inserts also include this simple yet attractive classic
+                    design for households. The timeless look to this free standing wood stove is amazing. Certified Wood
+                    Stove meets EPA requirements. Hampton Stoves works hard and looks great!
+                </p>
+                <br>
+                <p><strong>SPECIFICATIONS</strong></p>
+                <br>
+                <p><strong>Maximum</strong>(BTU) = 45,000</p>
+                <p><strong>Typical SQ.FT Heated</strong> = up to 1,000 sq.ft</p>
+                <p><strong>Optimum Efficiency</strong> = 83.6%</p>
+                <p><strong>Maximum Log Size</strong> = 18″</p>
+            </div>
+        </v-col>
+    </v-row>
+    <v-row class="pa-md-16 my-md-0 my-10">
+        <v-col md="12" cols="12">
+            <div class="flex flex-column pa-10">
+                <v-divider class="mb-4 w-100" color="green" style="border-top-width: 5px; opacity: 1;"></v-divider>
+                <p class="text-justify">Are you ready to have the experts at Chimcare install new wood burning fireplace
+                    inserts? Our experienced team will gladly use their skills to complete the installation precisely.
+                    Contact Chimneyz when you’re ready to learn more.
+
+                    Get a Free Estimate Now
+                </p>
+                <br>
+            </div>
+        </v-col>
+    </v-row>
+    <v-row class="pa-md-16">
+        <v-col md="12" cols="12">
+            <h1 class="mb-5 text-center">Frequently Ask Question</h1>
+            <v-divider class="mb-4 w-33" color="green"
+                       style="border-top-width: 5px; opacity: 1; margin: 0 auto"></v-divider>
+            <v-expansion-panels>
+                <v-expansion-panel
+                    v-for="(item, index) in faqs"
+                    :key="index"
+                    :title="(index+1)+'. '+item.question"
+                    class="font-weight-bold"
+                    :text="item.answer"
+                ></v-expansion-panel>
+            </v-expansion-panels>
+        </v-col>
+    </v-row>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            faqs: [
+                {
+                    question: "How often should I have my chimney cleaned?",
+                    answer: "It's generally recommended to have your chimney cleaned at least once a year. However, if you use your fireplace frequently, more regular cleanings might be necessary to ensure safety and efficiency."
+                },
+                {
+                    question: "What are the signs that my chimney needs repairing?",
+                    answer: "Common signs include visible cracks in the masonry, water damage, a rusted damper or firebox, and spalling bricks. If you notice any of these signs, it's important to get a professional inspection as soon as possible."
+                },
+                {
+                    question: "Can I use my fireplace if my chimney is damaged?",
+                    answer: "It's not safe to use your fireplace if your chimney is damaged. Doing so can increase the risk of a chimney fire or carbon monoxide poisoning. We recommend scheduling a repair service before using your fireplace."
+                },
+                {
+                    question: "What does a chimney cleaning service involve?",
+                    answer: "Our chimney cleaning service includes removing soot and creosote from the chimney liner, firebox, smoke chamber, and damper. We also perform a visual inspection to check for any potential issues."
+                },
+                {
+                    question: "How long does a chimney repair or cleaning take?",
+                    answer: "Most chimney cleanings and minor repairs can be completed in one to two hours. More extensive repairs might take longer, and we can provide a time estimate after an initial inspection."
+                },
+            ],
+            items: [
+                {
+                    src: '../images/gas_log_1.jpg',
+                },
+                {
+                    src: '../images/gas_log_2.webp',
+                },
+                {
+                    src: '../images/gas_log_3.jpg',
+                },
+                {
+                    src: '../images/gas_log_4.jpg',
+                },
+            ],
+            determine_size: [
+                {
+                    text: "Measure the front width of you fireplace"
+                },
+                {
+                    text: "Measure the width at the back of your fireplace"
+                },
+                {
+                    text: "Measure the depth from the front to the back of your fireplace"
+                },
+                {
+                    text: "Measure the height from the top to bottom of the front your fireplace"
+                },
+                {
+                    text: "Measure the height from the top to bottom of the back your fireplace"
+                },
+            ],
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
