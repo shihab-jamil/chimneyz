3ea96205dfe42099d359903fa3219e31d66d930d
emon <ishtiaqueferdous109@gmail.com>
2024-01-29 19:27:05 +0600

    Gas Fireplace Insert Component Done

---
+++ resources/js/components/GasFireplaceInsertComponent.vue
@@ -0,0 +1,235 @@
+<template>
+    <v-row class="pa-md-16 my-md-0 my-10">
+        <v-col md="6" cols="12" class="d-flex justify-end align-center">
+            <v-img src="../images/gas_fireplace_insert_1.jpg" :aspect-ratio="9/5" rounded="xl"/>
+        </v-col>
+        <v-col md="6" cols="12">
+            <div class="flex flex-column pa-10">
+                <h1>Lets Talk About Fireplace Insert</h1>
+                <v-divider class="mb-4 w-100" color="green" style="border-top-width: 5px; opacity: 1;"></v-divider>
+                <p class="text-justify">Do you need a new gas fireplace insert to improve your indoor fireplace? If so,
+                    don’t hesitate to reach out to our experts for assistance today. You deserve to have a fireplace at
+                    home that looks and performs how you want. Chimcare sells and installs a wide variety of gas
+                    fireplace inserts at our Seattle, WA; Portland, OR; and Cleveland, OH locations. We carry a full
+                    line of Regency products with a wide range of sizes and looks to fit your specific needs. Continue
+                    to the list below to learn more about the inserts we have available to improve your fireplace
+                    today.</p>
+                <br>
+            </div>
+        </v-col>
+    </v-row>
+    <v-row class="pa-md-16 my-md-0 my-10">
+        <v-col md="6" cols="12">
+            <div class="flex flex-column pa-10">
+                <h1>Liberty ® Radiant Medium Gas Insert</h1>
+                <v-divider class="mb-4 w-100" color="green" style="border-top-width: 5px; opacity: 1;"></v-divider>
+                <p class="text-justify">Update your home with this beautiful gas insert. The Radiant Inserts deliver
+                    heat efficiency while providing a large viewing area of the fire. You can choose between having
+                    traditional logs, glass crystals or spa stones for the interior.
+                </p>
+                <br>
+                <p><strong>SPECIFICATIONS (Natural Gas)</strong></p>
+                <br>
+                <p><strong>Input</strong>(BTU) = 31,000</p>
+                <p><strong>Turn down to</strong>(BTU) = 22,000</p>
+                <p><strong>Efficiency</strong> = 84%</p>
+                <br>
+                <p><strong>Minimum Fireplace Opening</strong></p>
+                <br>
+                <p><strong>Width</strong>(Front) = 30″</p>
+                <p><strong>Width</strong>(Rear) = 22″</p>
+                <p><strong>Height</strong> = 20″</p>
+                <p><strong>Depth</strong> = 16″</p>
+            </div>
+        </v-col>
+        <v-col md="6" cols="12" class="d-flex justify-end align-center">
+            <v-img src="../images/rediant_gas.jpg" :aspect-ratio="9/5" rounded="xl"/>
+        </v-col>
+    </v-row>
+    <v-row class="pa-md-16 my-md-0 my-10">
+        <v-col md="6" cols="12" class="d-flex justify-end align-center">
+            <v-img src="../images/radient_gas_large.jpg" :aspect-ratio="9/5" rounded="xl"/>
+        </v-col>
+        <v-col md="6" cols="12">
+            <div class="flex flex-column pa-10">
+                <h1>Liberty ® Radiant Large Gas Insert</h1>
+                <v-divider class="mb-4 w-100" color="green" style="border-top-width: 5px; opacity: 1;"></v-divider>
+                <p class="text-justify">Update your home with this beautiful large gas insert. The Radiant Insert
+                    provides radiant & convective heat, and allows heat to be moved effortlessly throughout the room
+                    with a circulating fan.
+
+                    You can choose between having traditional logs, glass crystals or spa stones for the interior of
+                    firebox.
+                </p>
+                <br>
+                <p><strong>SPECIFICATIONS (Natural Gas)</strong></p>
+                <br>
+                <p><strong>Input</strong>(BTU) = 38,000</p>
+                <p><strong>Turn down to</strong>(BTU) = 25,000</p>
+                <p><strong>Efficiency</strong> = 84%</p>
+                <br>
+                <p><strong>Minimum Fireplace Opening</strong></p>
+                <br>
+                <p><strong>Width</strong>(Front) = 34″</p>
+                <p><strong>Width</strong>(Rear) = 25″</p>
+                <p><strong>Height</strong> = 24″</p>
+                <p><strong>Depth</strong> = 17″</p>
+            </div>
+        </v-col>
+    </v-row>
+    <v-row class="pa-md-16 my-md-0 my-10">
+        <v-col md="6" cols="12">
+            <div class="flex flex-column pa-10">
+                <h1>Energy Large Gas Insert</h1>
+                <v-divider class="mb-4 w-100" color="green" style="border-top-width: 5px; opacity: 1;"></v-divider>
+                <p class="text-justify">With 38,000 BTU’s of comfort, controllable heat this insert is guaranteed to
+                    warm your room quickly and consistently. This direct Vent Gas Insert is perfect for airtight homes
+                    or basements because the sealed firebox doesn’t use internal air.
+
+                    Includes remote control or wall thermostat.
+                </p>
+                <br>
+                <p><strong>SPECIFICATIONS (Natural Gas)</strong></p>
+                <br>
+                <p><strong>Input</strong>(BTU) = 38,000</p>
+                <p><strong>Turn down to</strong>(BTU) = 20,000</p>
+                <p><strong>Efficiency</strong> = 84%</p>
+                <br>
+                <p><strong>Minimum Fireplace Opening</strong></p>
+                <br>
+                <p><strong>Width</strong>(Front) = 33″</p>
+                <p><strong>Width</strong>(Rear) = 25″</p>
+                <p><strong>Height</strong> = 25″</p>
+                <p><strong>Depth</strong> = 17″</p>
+            </div>
+        </v-col>
+        <v-col md="6" cols="12" class="d-flex justify-end align-center">
+            <v-img src="../images/energy_large_gas.jpg" :aspect-ratio="9/5" rounded="xl"/>
+        </v-col>
+    </v-row>
+    <v-row class="pa-md-16 my-md-0 my-10">
+        <v-col md="6" cols="12" class="d-flex justify-end align-center">
+            <v-img src="../images/energy_medium_gas.jpg" :aspect-ratio="9/5" rounded="xl"/>
+        </v-col>
+        <v-col md="6" cols="12">
+            <div class="flex flex-column pa-10">
+                <h1>Energy Medium Gas Insert</h1>
+                <v-divider class="mb-4 w-100" color="green" style="border-top-width: 5px; opacity: 1;"></v-divider>
+                <p class="text-justify">This is a great affordable unit to convert your existing wood burning fireplace
+                    into a high efficiency gas fireplace. Enjoy peace of mind as all Regency gas inserts can operate
+                    without electricity!
+                </p>
+                <br>
+                <p><strong>SPECIFICATIONS (Natural Gas)</strong></p>
+                <br>
+                <p><strong>Input</strong>(BTU) = 30,000</p>
+                <p><strong>Turn down to</strong>(BTU) = 20,600</p>
+                <p><strong>Efficiency</strong> = 76%</p>
+                <br>
+                <p><strong>Minimum Fireplace Opening</strong></p>
+                <br>
+                <p><strong>Width</strong>(Front) = 27″</p>
+                <p><strong>Width</strong>(Rear) = 23″</p>
+                <p><strong>Height</strong> = 21″</p>
+                <p><strong>Depth</strong> = 15″</p>
+            </div>
+        </v-col>
+    </v-row>
+    <v-row class="pa-md-16 my-md-0 my-10">
+        <v-col md="12" cols="12">
+            <div class="flex flex-column pa-10">
+                <h1>What type of maintenance is required for a Gas Log Set?
+                </h1>
+                <v-divider class="mb-4 w-100" color="green" style="border-top-width: 5px; opacity: 1;"></v-divider>
+                <p class="text-justify">So which of the gas inserts below are right for your fireplace? If you are still
+                    unsure, we can help you find the best fit for your home whenever you’re ready. Contact our experts
+                    at Chimcare to learn how we can install the perfect insert to elevate your heating at home.
+
+                    Get a Free Estimate Now
+                </p>
+                <br>
+            </div>
+        </v-col>
+    </v-row>
+    <v-row class="pa-md-16">
+        <v-col md="12" cols="12">
+            <h1 class="mb-5 text-center">Frequently Ask Question</h1>
+            <v-divider class="mb-4 w-33" color="green"
+                       style="border-top-width: 5px; opacity: 1; margin: 0 auto"></v-divider>
+            <v-expansion-panels>
+                <v-expansion-panel
+                    v-for="(item, index) in faqs"
+                    :key="index"
+                    :title="(index+1)+'. '+item.question"
+                    class="font-weight-bold"
+                    :text="item.answer"
+                ></v-expansion-panel>
+            </v-expansion-panels>
+        </v-col>
+    </v-row>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            faqs: [
+                {
+                    question: "How often should I have my chimney cleaned?",
+                    answer: "It's generally recommended to have your chimney cleaned at least once a year. However, if you use your fireplace frequently, more regular cleanings might be necessary to ensure safety and efficiency."
+                },
+                {
+                    question: "What are the signs that my chimney needs repairing?",
+                    answer: "Common signs include visible cracks in the masonry, water damage, a rusted damper or firebox, and spalling bricks. If you notice any of these signs, it's important to get a professional inspection as soon as possible."
+                },
+                {
+                    question: "Can I use my fireplace if my chimney is damaged?",
+                    answer: "It's not safe to use your fireplace if your chimney is damaged. Doing so can increase the risk of a chimney fire or carbon monoxide poisoning. We recommend scheduling a repair service before using your fireplace."
+                },
+                {
+                    question: "What does a chimney cleaning service involve?",
+                    answer: "Our chimney cleaning service includes removing soot and creosote from the chimney liner, firebox, smoke chamber, and damper. We also perform a visual inspection to check for any potential issues."
+                },
+                {
+                    question: "How long does a chimney repair or cleaning take?",
+                    answer: "Most chimney cleanings and minor repairs can be completed in one to two hours. More extensive repairs might take longer, and we can provide a time estimate after an initial inspection."
+                },
+            ],
+            items: [
+                {
+                    src: '../images/gas_log_1.jpg',
+                },
+                {
+                    src: '../images/gas_log_2.webp',
+                },
+                {
+                    src: '../images/gas_log_3.jpg',
+                },
+                {
+                    src: '../images/gas_log_4.jpg',
+                },
+            ],
+            determine_size: [
+                {
+                    text: "Measure the front width of you fireplace"
+                },
+                {
+                    text: "Measure the width at the back of your fireplace"
+                },
+                {
+                    text: "Measure the depth from the front to the back of your fireplace"
+                },
+                {
+                    text: "Measure the height from the top to bottom of the front your fireplace"
+                },
+                {
+                    text: "Measure the height from the top to bottom of the back your fireplace"
+                },
+            ],
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
